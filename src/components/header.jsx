0bbd90c7ea5dff9d29ef79cbb34bb916b2c7499c
AdelC57 <adelcamilleri57@gmail.com>
2026-01-21 14:44:23 +0100

    Création du composant Header

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Trouve ton artisan</h1>
+            <p>Avec la région Auvergne-Rhône-Alpes</p>
+        </header>
+    );
+}
+
+export default Header;
